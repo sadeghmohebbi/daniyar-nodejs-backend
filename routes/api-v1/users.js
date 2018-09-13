3d6346b8f95e9c04a88ea1f068260b53f590aaed
sadegh mohebbi <mohebbi.sadegh@gmail.com>
2018-09-13 23:15:15 +0430

    auth not required in login

--- routes/api-v1/users.js
+++ routes/api-v1/users.js
@@ -3,10 +3,10 @@ const auth = require('../middlewares/auth');
 const { check } = require('express-validator/check');
 const usersController = require('../../controllers/users');
 
-router.post('/', usersController.check_items, auth.optional, usersController.register_new_user);
+router.post('/', usersController.check_items, usersController.register_new_user);
 
 //POST login route (optional, everyone has access)
-router.post('/login', usersController.check_items, auth.optional, usersController.login);
+router.post('/login', usersController.check_items, usersController.login);
 
 //GET me route (required, only authenticated user have access)
 router.get('/me', auth.required, usersController.get_current_user_detail);
