aa558ff6292dbe2dbc356e2645e94726d21788b9
sadegh mohebbi <mohebbi.sadegh@gmail.com>
2018-11-13 04:26:50 +0330

    config js reverted

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: "qvzkyXwSfZfbKYpsHGrCjvxwHXUqJXDLGQCLwtSS",
+    GOOGLE_CLIENT_ID: "296337488937-bo16kjijlj4jpuion1t9p9t5n9l12sdh.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "TmGAXXO-lVUANKzZl7FNcRRC",
+    domain: "https://serat-nodejs-backend.herokuapp.com/"
+}
